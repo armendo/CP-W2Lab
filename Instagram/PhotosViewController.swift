d5e191dd133f596172c71d37ed7ed1d845653668
armendo <Fer@Fers-MacBook-Pro.local>
2016-01-28 20:34:28 -0500

    Code
    Code

---
+++ Instagram/PhotosViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Fer on 1/28/16.
+//  Copyright © 2016 Fernando Mendoza. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var requestData    =   NSArray()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate  =   self
+        tableView.dataSource    =   self
+        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+//                            NSLog("response: \(responseDictionary)")
+                            self.requestData    =   responseDictionary["data"] as! NSArray
+                            self.tableView.reloadData()
+                            
+                    }
+                }
+        });
+        task.resume()
+        
+        
+        }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return requestData.count
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+//    self.requestData["data"]![0]["likes"]!!["data"]!![0]["profile_picture"]
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! PhotoCell
+        let profile          =   requestData[indexPath.row]["likes"]!!["data"]!![0]["profile_picture"] as! String
+        let imageUrl    = NSURL(string: profile)
+
+        print(profile)
+//        let posterPath  = movie["poster_path"] as! String
+//        let baseUrl = "http://image.tmdb.org/t/p/w342"
+//        
+//        let imageUrl    = NSURL(string: baseUrl + posterPath)
+//        
+//        cell.movieImage.setImageWithURL(imageUrl!)
+//        
+//        
+//        
+        cell.photo.setImageWithURL(imageUrl!)
+        return cell
+    }
+    
+    
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# Uncomment this line if you're using Swift
+
+platform :ios, '8.0'
+use_frameworks!    # Instruct CocoaPods to use dynamic frameworks instead of static libraries for Pods
+
+target ‘Instagram’ do
+
+# This pulls in the latest version of AFNetworking >= 2.6 but less than 3.0.
+pod 'AFNetworking', '~> 3.0'
+
+end
